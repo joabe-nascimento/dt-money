ca529706a150c026e890867387d9ddc1621288cc
Joabe <joabefnascimento1@outlook.com>
2024-03-24 14:22:52 -0300

    finalzação da estrutura visual

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
+import * as Dialog from "@radix-ui/react-dialog";
 import logoimg from "../../assets/logo..svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoimg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.DialogTrigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
