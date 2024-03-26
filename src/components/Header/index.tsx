f567452bdb624a827b6d3b3a2266629d1e2b9ecf
Joabe <joabefnascimento1@outlook.com>
2024-03-26 16:02:46 -0300

    links do git e linkedin

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,34 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
-
-import * as Dialog from '@radix-ui/react-dialog'
-import logoimg from '../../assets/logo..svg'
-import { NewTransactionModal } from '../NewTransactionModal'
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import logoimg from "../../assets/logo..svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoimg} alt="" />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <a
+            href="https://github.com/joabe-nascimento"
+            target="_blank"
+            title="Joabe Nascimento"
+            rel="noopener noreferrer"
+            style={{ marginRight: "10px", color: "white" }} // Definindo a cor como branca
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/joabe-nascimento-632345211/"
+            target="_blank"
+            title="Joabe Nascimento"
+            rel="noopener noreferrer"
+            style={{ color: "white" }} // Definindo a cor como branca
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
         <Dialog.Root>
           <Dialog.DialogTrigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
@@ -17,5 +37,5 @@ export const Header = () => {
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
-  )
-}
+  );
+};
